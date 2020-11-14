42b57f7059bb11596ec52fff771500bd32768f33
andrei.gaina <gandrei5@yahoo.com>
2020-11-14 17:47:44 +0200

    UserInterface

---
+++ Project/main.py
@@ -0,0 +1,24 @@
+
+import os
+import sys
+from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtWidgets import *
+from PyQt5.uic import loadUi
+
+
+class MainWindow(QMainWindow):
+    ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
+
+    def __init__(self):
+        super(MainWindow, self).__init__()
+        ui_path = os.path.join(self.ROOT_DIR, 'UserInterface.ui')
+        print(ui_path)
+        loadUi(ui_path, self)
+        self.file_path = None
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
+    sys.exit(app.exec_())
