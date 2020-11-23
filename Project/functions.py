1ac0a39f4a8cf6790d7eba50324f7b2ba1d5c576
andrei.gaina <gandrei5@yahoo.com>
2020-11-23 19:48:55 +0200

    New Update

---
+++ Project/functions.py
@@ -0,0 +1,18 @@
+import socket
+import time
+
+
+def current_time_millis():
+    """Current system time in milliseconds"""
+    return time.time() * 1000
+
+
+def new_socket():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    # More than one process needs to bind to the same SOCK_DGRAM port =>
+    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
+    # Unrestricted in scope
+    s.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, 255)
+    # Enable loopback
+    s.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_LOOP, 1)
