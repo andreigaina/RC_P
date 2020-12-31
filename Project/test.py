55d19b6c3b8965dd95995887dce8871a87fe8a3d
andrei.gaina <gandrei5@yahoo.com>
2020-12-31 22:01:54 +0200

    Testare de revelion:)

---
+++ Project/test.py
@@ -0,0 +1,40 @@
+from zeroconf import Zeroconf
+from pieces import *
+
+
+class MyListener(object):
+
+    def remove_service(self, zeroconf, type, name):
+        print("Service %s removed" % (name,))
+        print('\n')
+
+    def add_service(self, zeroconf, type, name):
+        print("Service %s added" % (name,))
+        print("  Type is %s" % (type,))
+        info = zeroconf.get_service_info(type, name)
+        #print(info)
+        if info:
+            print("  Address is %s:%d" % (socket.inet_ntoa(info.address),
+                                          info.port))
+            print("  Weight is %d, Priority is %d" % (info.weight,
+                                                      info.priority))
+            print("  Server is", info.server)
+            if info.properties:
+                print("  Properties are")
+                for key, value in info.properties.items():
+                    print("    %s: %s" % (key, value))
+        else:
+            print("  No info")
+        print('\n')
+
+
+if __name__ == '__main__':
+
+    zeroconf = Zeroconf()
+    print("Browsing services...")
+    listener = MyListener()
+    browser = ServiceBrowser(zeroconf, "_http._tcp.local.", listener)
+    try:
+        input("Waiting (press Enter to exit)...\n\n")
+    finally:
+        zeroconf.close()
