25900bcb8e732f007d531cb14b040a0ce61bbfe1
andrei.gaina <gandrei5@yahoo.com>
2021-01-05 14:19:23 +0200

    Class RegisteServicePopUp

--- Project/main.py
+++ Project/main.py
@@ -13,7 +13,44 @@ class MainWindow(QMainWindow):
         print(ui_path)
         loadUi(ui_path, self)
         self.file_path = None
+        self.registerServicePopUp = RegisterServicePopUp(self)
+        self.addServiceButton.clicked.connect(self.registerServicePopUp.show)
         # self.outputDisplay.appendPlainText("ndsfffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffdsssss")
+
+
+class RegisterServicePopUp(QDialog):
+    ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
+
+    def __init__(self, parent):
+        super().__init__(parent=parent)
+        self.ui_path = os.path.join(self.ROOT_DIR, 'register.ui')
+        loadUi(self.ui_path, self)
+        self.file_path = None
+        self.cancelButton.clicked.connect(self.delete_close)
+
+    def delete_close(self):
+        self.typeEdit.clear()
+        self.nameEdit.clear()
+        self.addressEdit.clear()
+        self.portEdit.clear()
+        self.weightEdit.clear()
+        self.priorityEdit.clear()
+        self.ttlEdit.clear()
+        self.serverEdit.clear()
+        self.close()
+
+    @staticmethod
+    def isSignalConnected1(obj, name):
+        index = obj.metaObject().indexOfMethod(name)
+        if index > -1:
+            method = obj.metaObject().method(index)
+            if method:
+                return obj.isSignalConnected(method)
+        return False
+
+    def __repr__(self):
+        return "STUDENTI"
+
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
